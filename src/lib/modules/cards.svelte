3e6e7655e74ba53da269858eeb7b66bf8f5f5cd6
Trey Willetto <trey.willetto@gmail.com>
2024-03-29 15:14:40 -0600

    Update colors and replace in components.

--- src/lib/modules/cards.svelte
+++ src/lib/modules/cards.svelte
@@ -1,110 +1,112 @@
 <script lang="ts">
-  import drFew from '$lib/imgs/dr-few.png'
-  import rule29 from '$lib/imgs/rule29.png'
-  import inHire from '$lib/imgs/inhire.png'
+	import drFew from '$lib/imgs/dr-few.png';
+	import rule29 from '$lib/imgs/rule29.png';
+	import inHire from '$lib/imgs/inhire.png';
 </script>
 
-
 <div class="cards">
-  <div class="card">
-    <a href="http://www.drfewskincare.com" target="_blank">
-    <img class="card-image" src={drFew} alt="Dr. Few Website">
-    <p class="h3">Dr. Few Skincare</p>
-    <p class="code">Shopify Custom Theme</p>
-    <p class="label">Role: Lead Developer</p>
-  </a>
-    <p class="label">Design Agency: <a class="inline-link" href="https://hellogrip.com/" target="_blank">GRIP</a></p>
-  </div>
-  <div class="card">
-    <a href="http://www.rule29.com" target="_blank">
-      <img class="card-image" src={rule29} alt="Rule29 Website">
-      <p class="h3">Rule29 Rebrand</p>
-      <p class="code">Headless CMS + SvelteKit</p>
-      <p class="label">Role: Lead Developer</p>
-      <p class="label">Design Agency: Rule29</p>
-    </a>
-  </div>
-  <div class="card">
-    <a href="https://inhire.infolk.co/?email=demo@infolk.co&password=@Test12345" target="_blank">
-
-    <img class="card-image" src={inHire} alt="InHire Website">
-    <p class="h3">InHire App</p>
-    <p class="code">SvelteKit + Supabase</p>
-    <p class="label">Role: Front-End Developer</p>
-    <p class="label">Design Agency: Rule29</p>
-    </a>
-  </div>
+	<div class="card">
+		<a href="http://www.drfewskincare.com" target="_blank">
+			<img class="card-image" src={drFew} alt="Dr. Few Website" />
+			<p class="h3">Dr. Few Skincare</p>
+			<p class="code">Shopify Custom Theme</p>
+			<p class="label">Role: Lead Developer</p>
+		</a>
+		<p class="label">
+			Design Agency: <a class="inline-link" href="https://hellogrip.com/" target="_blank">GRIP</a>
+		</p>
+	</div>
+	<div class="card">
+		<a href="http://www.rule29.com" target="_blank">
+			<img class="card-image" src={rule29} alt="Rule29 Website" />
+			<p class="h3">Rule29 Rebrand</p>
+			<p class="code">Headless CMS + SvelteKit</p>
+			<p class="label">Role: Lead Developer</p>
+			<p class="label">Design Agency: Rule29</p>
+		</a>
+	</div>
+	<div class="card">
+		<a href="https://inhire.infolk.co/?email=demo@infolk.co&password=@Test12345" target="_blank">
+			<img class="card-image" src={inHire} alt="InHire Website" />
+			<p class="h3">InHire App</p>
+			<p class="code">SvelteKit + Supabase</p>
+			<p class="label">Role: Front-End Developer</p>
+			<p class="label">Design Agency: Rule29</p>
+		</a>
+	</div>
 </div>
 
 <style>
-  .cards {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1.5rem;
-  width: 100%;
-}
+	.cards {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1.5rem;
+		width: 100%;
+	}
 
-.cards > * {
-  flex-basis: calc(( 50rem - 100%) * 999);
-  flex-grow: 1;
-}
+	.cards > * {
+		flex-basis: calc((50rem - 100%) * 999);
+		flex-grow: 1;
+	}
 
-.cards > :nth-last-child(n+ 5),
-.cards > :nth-last-child(n+ 5) ~ * {
-  flex-basis: 100%;
-}
-.card{
-  padding: 1rem;
+	.cards > :nth-last-child(n + 5),
+	.cards > :nth-last-child(n + 5) ~ * {
+		flex-basis: 100%;
+	}
+	.card {
+		padding: 1rem;
 
-  text-align: left;
+		text-align: left;
 
-  background-color: var(--c-black-o1);
-  backdrop-filter: blur(5px);
-  border: 1px solid var(--c-white-o1);
-  border-radius: 5px;
-  box-shadow: 0 0 30px -10px var(--c-black-o9);
+		background-color: oklch(var(--lch-neutral-900) / 0.1);
+		backdrop-filter: blur(5px);
+		border: 1px solid oklch(var(--lch-neutral-100) / 0.1);
+		border-radius: 5px;
+		box-shadow: 0 0 30px -10px var(--c-neutral-800);
 
-  transition: background 0.15s ease-in-out, backdrop-filter 0.15s ease-in-out;
-  & a{
-    color: inherit;
-    text-decoration: none;
-  }
-  &:hover, &:focus{
-    background-color: var(--c-white-o1);
-    backdrop-filter: blur(10px);
-  }
-}
+		transition:
+			background 0.15s ease-in-out,
+			backdrop-filter 0.15s ease-in-out;
+		& a {
+			color: inherit;
+			text-decoration: none;
+		}
+		&:hover,
+		&:focus {
+			background-color: oklch(var(--lch-neutral-100) / 0.1);
+			backdrop-filter: blur(10px);
+		}
+	}
 
-.card-image{
-  margin-bottom: 1rem;
-  border-radius: 3px;
+	.card-image {
+		margin-bottom: 1rem;
+		border-radius: 3px;
+	}
 
-}
+	.code {
+		margin-bottom: 1rem;
+		font-family: 'Red Hat Mono', 'Courier New', monospace;
+		font-size: 1rem;
+		line-height: 1.5;
+	}
 
-
-
-.code {
-  margin-bottom: 1rem;
-	font-family: 'Red Hat Mono', 'Courier New', monospace;
-	font-size: 1rem;
-	line-height: 1.5;
-}
-
-.label {
-	font-family: Georgia, 'Times New Roman', Times, serif;
-	font-style: italic;
-}
-a.inline-link{
-  padding: 2px 3px;
-  text-decoration: underline;
-  border-radius: 2px;
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out;
-  &:hover, &:focus{
-    font-style: normal;
-    color: #e65737;
-    text-decoration: none;
-    background-color: var(--c-white);
-  }
-}
+	.label {
+		font-family: Georgia, 'Times New Roman', Times, serif;
+		font-style: italic;
+	}
+	a.inline-link {
+		padding: 2px 3px;
+		text-decoration: underline;
+		border-radius: 2px;
+		transition:
+			color 0.15s ease-in-out,
+			background-color 0.15s ease-in-out;
+		&:hover,
+		&:focus {
+			font-style: normal;
+			color: #e65737;
+			text-decoration: none;
+			background-color: var(--c-neutral-100);
+		}
+	}
 </style>
-
--- src/lib/modules/coming-soon.svelte
+++ src/lib/modules/coming-soon.svelte
@@ -1,140 +1,139 @@
 <script lang="ts">
-    import garden from '$lib/imgs/garden-bg.jpg';
+	import garden from '$lib/imgs/garden-bg.jpg';
 	import Cards from './cards.svelte';
 	let scroll: number = 0;
-  let parallaxElement: HTMLElement; 
+	let parallaxElement: HTMLElement;
 </script>
 
 <svelte:window bind:scrollY={scroll} />
 
 <div class="coming-soon-wrapper">
-  <div class="background parallax" bind:this={parallaxElement}>
-    <img src={garden} alt="" style={`transform: translateY(${scroll * -.2}px);`}>
-  </div>
-  <div class="center">
-    <h2 class="h2">Welcome!</h2>
-    <p class="p">
-      I'm currently overhauling my site. In the meantime, here are a few links to my recent work:
-    </p>
-    <Cards/>
-  </div>
+	<div class="background parallax" bind:this={parallaxElement}>
+		<img src={garden} alt="" style={`transform: translateY(${scroll * -0.2}px);`} />
+	</div>
+	<div class="center">
+		<h2 class="h2">Welcome!</h2>
+		<p class="p">
+			I'm currently overhauling my site. In the meantime, here are a few links to my recent work:
+		</p>
+		<Cards />
+	</div>
 </div>
 
 <style>
-.coming-soon-wrapper {
-  --local-bg-color: var(--c-white);
-  --local-font-color: var(--c-white);
-  --local-bg-opacity: 0.4;
+	.coming-soon-wrapper {
+		--local-bg-color: var(--c-neutral-100);
+		--local-font-color: var(--c-neutral-100);
+		--local-bg-opacity: 0.4;
 
-  position: relative;
-  z-index: 0;
+		position: relative;
+		z-index: 0;
 
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+		overflow: hidden;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 
-  min-block-size: 70vh;
-  padding: 5rem 3rem;
+		min-block-size: 70vh;
+		padding: 5rem 3rem;
 
-  color: var(--local-font-color);
+		color: var(--local-font-color);
 
-  background-color: var(--local-bg-color);
-  /* background-image: var(--texture-url); */
-  background-repeat: no-repeat;
-  background-size: cover;
-  &::before {
-    /* Drop Shadow */
-    pointer-events: none;
-    content: "";
+		background-color: var(--local-bg-color);
+		/* background-image: var(--texture-url); */
+		background-repeat: no-repeat;
+		background-size: cover;
+		&::before {
+			/* Drop Shadow */
+			pointer-events: none;
+			content: '';
 
-    position: absolute;
-    z-index: -1;
-    top: -1.5rem;
-    left: 0;
+			position: absolute;
+			z-index: -1;
+			top: -1.5rem;
+			left: 0;
 
-    width: 100%;
-    height: 2.5rem;
+			width: 100%;
+			height: 2.5rem;
 
-    opacity: 0.4;
-    background-image: linear-gradient(180deg, var(--c-black) 0%, transparent 98% 0);
-    mix-blend-mode: multiply;
-  }
+			opacity: 0.4;
+			background-image: linear-gradient(180deg, var(--c-black) 0%, transparent 98% 0);
+			mix-blend-mode: multiply;
+		}
 
-  &::after {
-    /* BG Image */
-    pointer-events: none;
-    content: "";
+		&::after {
+			/* BG Image */
+			pointer-events: none;
+			content: '';
 
-    position: absolute;
-    z-index: -1;
-    top: 0;
-    left: 0;
+			position: absolute;
+			z-index: -1;
+			top: 0;
+			left: 0;
 
-    width: 100%;
-    height: 100%;
+			width: 100%;
+			height: 100%;
 
-    opacity: var(--local-bg-opacity);
-    background-color: var(--c-black);
-    mix-blend-mode: multiply;
+			opacity: var(--local-bg-opacity);
+			background-color: var(--c-neutral-900);
+			mix-blend-mode: multiply;
 
-    transition: opacity 0.3s ease-in-out;
-  }
-}
-.background{
-  position: absolute;
-  z-index: -2;
-  inset: 0;
+			transition: opacity 0.3s ease-in-out;
+		}
+	}
+	.background {
+		position: absolute;
+		z-index: -2;
+		inset: 0;
 
-  overflow: hidden;
+		overflow: hidden;
 
-  margin-bottom: 0;
-  & img{
-    /* object-position: top; */
-    width: 100%;
-    height: 120%;
-    object-fit: cover;
-    object-position: top;
-  } 
-}
+		margin-bottom: 0;
+		& img {
+			/* object-position: top; */
+			width: 100%;
+			height: 120%;
+			object-fit: cover;
+			object-position: top;
+		}
+	}
 
+	.coming-soon-wrapper > :first-child:not(.center) {
+		margin-block-start: 0;
+	}
 
-.coming-soon-wrapper > :first-child:not(.center) {
-  margin-block-start: 0;
-}
+	.coming-soon-wrapper > :last-child:not(.center) {
+		margin-block-end: 0;
+	}
 
-.coming-soon-wrapper > :last-child:not(.center) {
-  margin-block-end: 0;
-}
+	.coming-soon-wrapper > .center {
+		width: 100%;
+		margin-block: auto;
+	}
 
-.coming-soon-wrapper > .center {
-  width: 100%;
-  margin-block: auto;
-}
+	p,
+	h2 {
+		max-width: 40ch;
+		margin-inline: auto;
+		margin-inline-start: 0;
 
-p, h2{
-  max-width: 40ch;  
-  margin-inline: auto;
-margin-inline-start: 0;
+		text-align: left;
+		text-wrap: balance;
+		text-wrap: pretty;
+	}
+	h2 {
+		margin-block-end: 1rem;
+		color: var(--c-);
+	}
+	p {
+		margin-block-end: 2rem;
+	}
 
-  text-align: left;
-  text-wrap: balance;
-  text-wrap: pretty;
-}
-h2 {
-  margin-block-end: 1rem;
-  color: var(--c-);
-}
-p {
-  margin-block-end: 2rem;
-}
-
-@media (prefers-color-scheme: dark) {
-    .coming-soon-wrapper {
-      --local-bg-color: var(--c-green);
-      --local-font-color: var(--c-white-o9);
-      --local-bg-opacity: 0.6;
-
-    }
-  }
+	@media (prefers-color-scheme: dark) {
+		.coming-soon-wrapper {
+			--local-bg-color: var(--c-green-900);
+			--local-font-color: var(--c-neutral-200);
+			--local-bg-opacity: 0.6;
+		}
+	}
 </style>
